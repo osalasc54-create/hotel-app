2669557453b26af22244322e2298b37c24568e52
osalasc54-create <osalasc54@gmail.com>
2026-02-28 00:46:38 -0600

    correccion ortografica

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/auth.middleware');
-const paymentController = require('../controllers/payments.controller');
+const paymentController = require('../controllers/payment.controller');
 
 router.post(
   '/create-payment-intent',
